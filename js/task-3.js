033a89ee4a6ab0bd7160ba85400be073d2204b43
Iryna2512 <152912307+Iryna2512@users.noreply.github.com>
2024-03-23 00:19:29 +0200

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,20 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
+function checkForSpam(message) {
+  const forbiddenWord1 = "spam";
+  const forbiddenWord2 = "sale";
+  if (
+    message.toLowerCase().includes(forbiddenWord1) ||
+    message.toLowerCase().includes(forbiddenWord2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
